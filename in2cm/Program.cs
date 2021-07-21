f4fff2346b0c3ecf83259b7d53f1a62e77d4b7db
Valentin Vakrilov <vvakrilov@gmail.com>
2021-07-21 21:45:41 +0300

    Add project files.

---
+++ in2cm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace in2cm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inch = double.Parse(Console.ReadLine());
+            var cm = inch * 2.54;
+            Console.WriteLine(cm);
+        }
+    }
+}
